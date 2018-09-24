02ece42e04cbb15e930ac8456fc67ac016e738fa
TJJiang <tjiang@apple.com>
2018-09-24 15:51:09 -0700

    labels and color scheme working

--- Q4/heatmap.js
+++ Q4/heatmap.js
@@ -1,8 +1,7 @@
 var buckets = 9
-var colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"] // alternatively colorbrewer.YlGnBu[9]
-var margin = {top: 20, right: 90, bottom: 170, left: 50},
-    width = 600 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom,
+var margin = {top: 120, right: 90, bottom: 170, left: 150},
+    width = 700 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom,
     gridSize = Math.floor(width / 8);
 
 var svg = d3.select("body").append("svg")
@@ -35,8 +34,33 @@ d3.csv("heatmap.csv", function(rows) {
 
   x = d3.scale.ordinal().domain(spellorder).rangePoints([0, width]);
   y = d3.scale.ordinal().domain(bookorder).rangePoints([height, 0]);
+
+  // Discrete color array acquired from https://bl.ocks.org/mbostock/5577023
+  var colors = ["#fff5f0","#fee0d2","#fcbba1","#fc9272","#fb6a4a","#ef3b2c","#cb181d","#a50f15","#67000d"].reverse()
   z = d3.scale.quantile();
   z.domain([0, buckets-1, d3.max(dataset1, function(d) { return d.count; })]).range(colors);
+
+  var bookLabels = svg.selectAll(".bookLabel")
+      .data(bookorder)
+      .enter().append("text")
+        .text(function (d) { return d; })
+        .attr("x", 0)
+        .attr("y", function (d, i) { return i * gridSize * 1.2; })
+        .style("text-anchor", "end")
+        .attr("transform", "translate(-6," + gridSize / 1.8 + ")");
+
+  var spellLabels = svg.selectAll(".spellLabel")
+      .data(spellorder)
+      .enter().append("text")
+        .text(function(d) {return d;})
+        .attr("x", 0)
+        .attr("y", function(d, i) {
+            return (i * gridSize);
+        })
+        .style("text-anchor", "left")
+        .attr("transform", function(d, i) {
+          return "translate(" + (gridSize+16*i) / 2 + ", -6) rotate(-90) rotate(45, 0, " + (i * gridSize) + ")";
+      });
 
   // Display the tiles for each non-zero row.
   svg.selectAll(".tile")
@@ -66,35 +90,4 @@ d3.csv("heatmap.csv", function(rows) {
   //     .attr("y", 10)
   //     .attr("dy", ".35em")
   //     .text(String);
-
-  svg.append("text")
-      .attr("class", "label")
-      .attr("x", width + 20)
-      .attr("y", 10)
-      .attr("dy", ".35em")
-      .text("Count");
-
-  // Add an x-axis with label.
-  // svg.append("g")
-  //     .attr("class", "x axis")
-  //     .attr("transform", "translate(0," + height + ")")
-  //     .call(d3.svg.axis().scale(x).orient("bottom"))
-  //   .append("text")
-  //     .attr("class", "label")
-  //     .attr("x", width)
-  //     .attr("y", -6)
-  //     .attr("text-anchor", "end")
-  //     .text("Date");
-
-  // Add a y-axis with label.
-  // svg.append("g")
-  //     .attr("class", "y axis")
-  //     .call(d3.svg.axis().scale(y).orient("left"))
-  //   .append("text")
-  //     .attr("class", "label")
-  //     .attr("y", 6)
-  //     .attr("dy", ".71em")
-  //     .attr("text-anchor", "end")
-  //     .attr("transform", "rotate(-90)")
-  //     .text("Value");
 });
