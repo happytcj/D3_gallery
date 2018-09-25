d537196cb488535e3650c10681c1b989f209e372
TJJiang <tjiang@apple.com>
2018-09-25 13:15:58 -0700

    non-interactive horizontal barchart done

--- Q5/interactive.js
+++ Q5/interactive.js
@@ -1,34 +1,12 @@
-var data = [{
-        "name": "Apples",
-        "value": 20,
-},
-    {
-        "name": "Bananas",
-        "value": 12,
-},
-    {
-        "name": "Grapes",
-        "value": 19,
-},
-    {
-        "name": "Lemons",
-        "value": 5,
-},
-    {
-        "name": "Limes",
-        "value": 16,
-},
-    {
-        "name": "Oranges",
-        "value": 26,
-},
-    {
-        "name": "Pears",
-        "value": 30,
-}];
+var data=[{city: 'San Antonio', population_2012: 1383505, growth: {year_2013:25405, year_2014:26644 , year_2015:28593 , year_2016:23591 , year_2017:24208}},
+{city: 'New York', population_2012: 8383504, growth: {year_2013:75138 , year_2014:62493 , year_2015:61324 , year_2016:32967 , year_2017:7272}},
+{city: 'Chicago', population_2012: 2717989, growth: {year_2013:6493 , year_2014:2051 , year_2015:-1379 , year_2016:-4879 , year_2017:-3825}},
+{city: 'Los Angeles', population_2012: 3859267, growth:{year_2013:32516 , year_2014:30885 , year_2015:30791 , year_2016:27657 , year_2017:18643}},
+{city: 'Phoenix', population_2012: 1495880, growth: {year_2013:25302 , year_2014:26547 , year_2015:27310 , year_2016:27003 , year_2017:24036}}
+];
 
 //sort bars based on value
-data = data.sort(function (a, b) {
+data = data.sort(function (a, b, c,) {
     return d3.ascending(a.value, b.value);
 })
 
@@ -52,13 +30,13 @@ var svg = d3.select("#graphic").append("svg")
 var x = d3.scale.linear()
     .range([0, width])
     .domain([0, d3.max(data, function (d) {
-        return d.value;
+        return d.population_2012;
     })]);
 
 var y = d3.scale.ordinal()
     .rangeRoundBands([height, 0], .1)
     .domain(data.map(function (d) {
-        return d.name;
+        return d.city;
     }));
 
 //make y axis to show bar names
@@ -81,12 +59,12 @@ var bars = svg.selectAll(".bar")
 bars.append("rect")
     .attr("class", "bar")
     .attr("y", function (d) {
-        return y(d.name);
+        return y(d.city);
     })
     .attr("height", y.rangeBand())
     .attr("x", 0)
     .attr("width", function (d) {
-        return x(d.value);
+        return x(d.population_2012);
     });
 
 //add a value label to the right of each bar
@@ -94,12 +72,12 @@ bars.append("text")
     .attr("class", "label")
     //y position of the label is halfway down the bar
     .attr("y", function (d) {
-        return y(d.name) + y.rangeBand() / 2 + 4;
+        return y(d.city) + y.rangeBand() / 2 + 4;
     })
     //x position is 3 pixels to the right of the bar
     .attr("x", function (d) {
-        return x(d.value) + 3;
+        return x(d.population_2012) + 3;
     })
     .text(function (d) {
-        return d.value;
+        return d.population_2012;
     });
