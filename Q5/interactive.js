e468f5d3b41920cfe1173edb7c6c0b14918a9845
TJJiang <tjiang@apple.com>
2018-09-26 16:00:34 -0700

    bar ordering by population, scaffolding for interactive line chart

--- Q5/interactive.js
+++ Q5/interactive.js
@@ -5,21 +5,27 @@ var data=[{city: 'San Antonio', population_2012: 1383505, growth: {year_2013:254
 {city: 'Phoenix', population_2012: 1495880, growth: {year_2013:25302 , year_2014:26547 , year_2015:27310 , year_2016:27003 , year_2017:24036}}
 ];
 
-//sort bars based on value
-data = data.sort(function (a, b, c,) {
-    return d3.ascending(a.value, b.value);
-})
+function compare(a,b) {
+  if (a.population_2012 < b.population_2012) {
+    return -1;
+  }
+  else if (a.population_2012 > b.population_2012) {
+    return 1;
+  }
+  return 0;
+}
+data = data.sort(compare);
 
 //set up svg using margin conventions - we'll need plenty of room on the left for labels
 var margin = {
     top: 15,
     right: 25,
     bottom: 15,
-    left: 60
+    left: 80
 };
 
-var width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var width = 720 - margin.left - margin.right,
+    height = 375 - margin.top - margin.bottom;
 
 var svg = d3.select("#graphic").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -39,10 +45,8 @@ var y = d3.scale.ordinal()
         return d.city;
     }));
 
-//make y axis to show bar names
 var yAxis = d3.svg.axis()
     .scale(y)
-    //no tick marks
     .tickSize(0)
     .orient("left");
 
@@ -55,7 +59,88 @@ var bars = svg.selectAll(".bar")
     .enter()
     .append("g")
 
-//append rects
+var dataset1;
+function showLineChart(city, growth, pop2012) {
+    var margin = {top: 30, right: 20, bottom: 30, left: 50},
+        width = 336 - margin.left - margin.right,
+        height = 175 - margin.top - margin.bottom;
+
+    dataset1 = [];
+    var numYears = 5; 
+    for (var i = 0; i < numYears; i++) {
+        var obs = {}; 
+        obs.year = 2013+i
+        if (i === 0) {
+            obs.pop_pct_change = 100.0*(growth.year_2013-pop2012)/pop2012
+        } else if (i === 1) {
+            obs.pop_pct_change = 100.0*(growth.year_2014-growth.year_2013)/growth.year_2013
+        } else if (i === 2) {
+            obs.pop_pct_change = 100.0*(growth.year_2015-growth.year_2014)/growth.year_2014
+        } else if (i === 3) {
+            obs.pop_pct_change = 100.0*(growth.year_2016-growth.year_2015)/growth.year_2015
+        } else if (i === 4) {
+            obs.pop_pct_change = 100.0*(growth.year_2017-growth.year_2016)/growth.year_2016
+        }
+        dataset1.push(obs)
+    }
+
+    console.log(dataset1)
+    return
+
+    // Set the ranges
+    var x = d3.time.scale().range([0, width]);
+    var y = d3.scale.linear().range([height, 0]);
+
+    // Define the axes
+    var xAxis = d3.svg.axis().scale(x)
+        .orient("bottom").ticks(5);
+
+    var yAxis = d3.svg.axis().scale(y)
+        .orient("left").ticks(5);
+
+    // Define the line
+    var valueline = d3.svg.line()
+        .x(function(d) { return x(d.year); })
+        .y(function(d) { return y(d.close); });
+        
+    // Adds the svg canvas
+    var svg = d3.select("body")
+        .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", 
+                  "translate(" + margin.left + "," + margin.top + ")");
+
+    // Get the data
+    d3.csv("data.csv", function(error, data) {
+        growth.forEach(function(d) {
+            d.date = parseDate(d.date);
+            d.close = +d.close;
+        });
+
+        // Scale the range of the data
+        x.domain(d3.extent(data, function(d) { return d.date; }));
+        y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+        // Add the valueline path.
+        svg.append("path")
+            .attr("class", "line")
+            .attr("d", valueline(data));
+
+        // Add the X Axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        // Add the Y Axis
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+    });
+};
+
 bars.append("rect")
     .attr("class", "bar")
     .attr("y", function (d) {
@@ -63,20 +148,28 @@ bars.append("rect")
     })
     .attr("height", y.rangeBand())
     .attr("x", 0)
+    .attr("fill", "gray")
     .attr("width", function (d) {
         return x(d.population_2012);
+    })
+    .on("mouseover", function(d) {
+        d3.select(this).attr("fill", "red");
+        showLineChart(d.city, d.growth, d.population_2012)
+    })
+    .on("mouseout", function(d) {
+        d3.select(this).attr("fill", "gray");
     });
 
 //add a value label to the right of each bar
 bars.append("text")
-    .attr("class", "label")
+    .attr("class", "white-label")
     //y position of the label is halfway down the bar
     .attr("y", function (d) {
         return y(d.city) + y.rangeBand() / 2 + 4;
     })
     //x position is 3 pixels to the right of the bar
     .attr("x", function (d) {
-        return x(d.population_2012) + 3;
+        return 5;
     })
     .text(function (d) {
         return d.population_2012;
