e6c3ac10d2bb9b6174555a99d924fb98517c88a4
TJJiang <tjiang@apple.com>
2018-09-27 12:39:00 -0700

    tooltip dynamic repositioning

--- Q6/q6.js
+++ Q6/q6.js
@@ -3,6 +3,7 @@ var svg = d3.select("svg"),
     height = +svg.attr("height");
 
 var education = d3.map();
+var details = d3.map();
 
 var path = d3.geo.path();
 
@@ -12,16 +13,32 @@ var x = d3.scale.linear()
 
 d3.queue()
     .defer(d3.json, "us.json")
-    .defer(d3.csv, "education.csv", function(d) { education.set(d.id, +d.percent_educated); })
+    .defer(d3.csv, "education.csv", 
+      function(d) { 
+        var obs = {};
+        obs.id = d.id;
+        obs.percent_educated = +d.percent_educated;
+        obs.name = d.name.replace(/County/g, '');
+        education.set(d.id, obs);
+      })
+    .defer(d3.csv, "education_details.csv", 
+      function(d) { 
+        var obs = {};
+        if (d.id != "") {
+          obs.id = d.id;
+          obs.pro = +d.qualified_professionals.replace(/,/g, '');
+          obs.high = +d.high_school.replace(/,/g, '');
+          obs.mid = +d.middle_school_or_lower.replace(/,/g, '');
+          details.set(d.id, obs);
+        }
+
+      })
     .await(draw_choropleth);
 
-// var color_blues = ["#efedf5","#dadaeb","#bcbddc", "#9e9ac8","#8c96c6","#807dba","#6a51a3","#54278f","#3f007d", "#4d004b"]
 var color_blues = ['#e6e6fa','#dcceec','#d2b9de','#c7a1d0','#bc8bc3','#b174b5','#a55ea7','#99479a','#8d2c8d','#800080']
 var steps = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
 color = d3.scale.quantile();
 color.domain(steps).range(color_blues);
-
-// console.log(d3.extent(education, function(d) { return d.percent_educated; }))
 
 var g = svg.append("g")
     .attr("class", "key")
@@ -47,11 +64,11 @@ legend.append("rect")
 legend.append("text")
   .attr("class", "mono")
   .text(function(d,i) { 
-    console.log(steps[i]);
     return (steps[i]) + "%"; 
   })
   .attr("x", width-70)
   .attr("y", function(d, i) { return 50+gridSize * i; });
+// End Legend *******************************************************************************
 
 // Title
 g.append("text")
@@ -63,20 +80,56 @@ g.append("text")
     .attr("font-weight", "bold")
     .attr("font-size", 20)
     .text("Education Statistics");
-// End Legend *******************************************************************************
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset(function(d) {
+    coordinates = d3.mouse(this);
+    var x = -10;
+    var y = 0
+    if (coordinates[1] < 200) {
+      x = 100;
+    }
+    if (coordinates[0] < 300) {
+      y = 100
+    }
+    if (coordinates[0] > 600) {
+      y = -100
+    }
+    return [x, y];
+  })
+  .html(function(d) {
+    var tmp = education.get(d.id); 
+    var tmp2 = details.get(d.id); 
+    return "County: " + tmp.name + "<br />" + 
+        "Percent Educated: " + tmp.percent_educated + "%<br />" +
+        "Qualified Professionals: " + tmp2.pro + "%<br />" +
+        "High School Graduates: " + tmp2.high + "%<br />" +
+        "Middle School or Lower Graduates: " + tmp2.mid;
+  });
+
+svg.call(tip);
 
 function draw_choropleth(error, us) {
   if (error) {
     throw error;
   }
-
-  svg.append("g")
+// consulted following link for tooltips: http://bl.ocks.org/Caged/6476579
+choropleth = svg.append("g")
     .attr("class", "counties")
     .selectAll("path")
     .data(topojson.feature(us, us.objects.counties).features)
     .enter().append("path").attr("stroke-width", 0.25).attr("stroke", "black")
-    .attr("fill", function(d) { return color(d.percent_educated = education.get(d.id)); })
+
+choropleth.attr("fill", function(d,i) {
+  if (education.has(d.id)) {
+    return color(education.get(d.id).percent_educated); 
+  }
+})
     .attr("d", path)
-    .append("title")
-    .text(function(d) { return d.percent_educated + "%"; });
+    .append("title");
+
+choropleth
+    .on('mouseover', tip.show) 
+    .on('mouseout', tip.hide);
 }
